2467e9f561984ac06920db51cc6e15bb6bf7d95d
kaghog <kaghog@ethz.ch>
2021-10-19 17:01:24 +0200

    clean up code

--- src/main/java/abmt2021/exercise/week5/discreteModeChoice/modeChoice/AbmtModeChoiceModule.java
+++ src/main/java/abmt2021/exercise/week5/discreteModeChoice/modeChoice/AbmtModeChoiceModule.java
@@ -1,13 +1,8 @@
 package abmt2021.exercise.week5.discreteModeChoice.modeChoice;
 
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import org.eqasim.core.components.config.EqasimConfigGroup;
+
 import org.eqasim.core.simulation.mode_choice.AbstractEqasimExtension;
 import org.eqasim.core.simulation.mode_choice.parameters.ModeParameters;
-import org.eqasim.core.simulation.mode_choice.utilities.estimators.CarUtilityEstimator;
-import org.matsim.contribs.discrete_mode_choice.model.mode_availability.ModeAvailability;
-import org.matsim.core.config.CommandLine;
 
 import java.io.IOException;
 
@@ -16,8 +11,6 @@ import java.io.IOException;
  * @project abmt2021
  */
 public class AbmtModeChoiceModule extends AbstractEqasimExtension {
-
-    static public final String MODE_AVAILABILITY_NAME = "AbmtModeAvailability";
 
     @Override
     protected void installEqasimExtension() {
